1f9ea7218ba3f9f746737177c3e8c369a08c9d33
FelixPriori <felixriouxsabourin@gmail.com>
2020-02-04 19:42:20 +0000

    linted

--- countOnly.js
+++ countOnly.js
@@ -10,8 +10,8 @@ const assertEqual = function(actual, expected) {
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-  for (const item of allItems){
-    if (itemsToCount[item]) { 
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
       } else {
