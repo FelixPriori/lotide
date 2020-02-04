bb4503f74413ed4c00e0a44b68c6d0e974053292
FelixPriori <felixriouxsabourin@gmail.com>
2020-02-04 16:31:07 +0000

    implemented without()

---
+++ without.js
@@ -0,0 +1,42 @@
+const compareElements = function(e1, e2) {
+  if (e1 === e2) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  equals = true;
+  i = 0;
+  while (equals && i < arr1.length){
+    equals = compareElements(arr1[i], arr2[i]);
+    i++;
+  }
+  if (equals){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const without = function (collection, unwanted) {
+  let wanted = [];
+  for (let item of collection) {
+    if (unwanted.indexOf(item) === -1) {
+      wanted.push(item);
+    }
+  }
+  return wanted;
+};
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]) // => [2, 3]
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]) // => ["1", "2"]
