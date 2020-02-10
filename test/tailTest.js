317cac90aaa14ed4a6945eb2cf9b10d0054a8117
FelixPriori <felixriouxsabourin@gmail.com>
2020-02-10 15:56:02 +0000

    assert using chai instead of mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,8 @@
 const tail = require('../tail');
 const assert = require('chai').assert;
-const {expect} = require('chai');
 
 describe("#tail", () => {
   it("should return ['Lighthouse', 'Labs'] when passed ['Hi', 'Lighthouse', 'Labs']", () => {
-    expect(tail(['Hi', 'Lighthouse', 'Labs'])).to.eql(['Lighthouse', 'Labs']);
+    assert.deepEqual(tail(['Hi', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
   });
 });
