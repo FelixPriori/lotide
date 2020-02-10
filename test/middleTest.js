4b6496b3355444fae310fc06f79b9c0e0b38fbfc
FelixPriori <felixriouxsabourin@gmail.com>
2020-02-10 16:02:17 +0000

    mocha and chai tests with middle function

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,14 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-assertArraysEqual(middle([1, 2]), []);
-
-assertArraysEqual(middle([1]), []);
+describe("#middle", () => {
+  it("should give [2, 3] when passed [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("should give [2] when passed [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("should give [] when passed [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("should give [] when passed [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+});
