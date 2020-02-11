675aaafb3839741b62b5b1f601479e1ebfccbd28
FelixPriori <felixriouxsabourin@gmail.com>
2020-02-11 18:54:11 +0000

    refactored test with mocha/chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,12 +1,26 @@
-const assertEqual = require('../assertEqual');
-const eqArrays = require('../eqArrays')
+const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+describe("#eqArrays", () => {
+  it("should return false when two arrays have different lengths", () => {
+    assert.equal(eqArrays([1, 2], [1, 2, 3]), false);
+  });
+  it("should return true when two arrays of numbers are completely identical", () => {
+    assert.equal(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("should return false when two arrays of numbers are different", () => {
+    assert.equal(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+  it("should return true when two arrays of string of numbers are completely identical", () => {
+    assert.equal(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+  it("should return false when two arrays have a different type (but same numbers)", () => {
+    assert.equal(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+  it("should return false when two arrays with different levels but same value are equal", () => {
+    assert.equal(eqArrays([[2], [3, [4]]], [[2, 3], [4]]), false);
+  });
+  it("should return true when two arrays with nested arrays have the same values", () => {
+    assert.equal(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  });
+});
